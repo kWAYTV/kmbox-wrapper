6ec8c321b3419a04e664268c098f8e7627703d41
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-05-19 08:07:45 +0200

    Fix initialization parameters in main.cpp

--- kmbox/Source/main.cpp
+++ kmbox/Source/main.cpp
@@ -4,17 +4,16 @@ int main()
 {
     SetConsoleTitleA("Debug Console");
 
-    std::string IP = "192.168.1.188";
-    WORD Port = 0;
-    std::string UUID = "ABCDEFG";
-
-    if (Kmbox.InitDevice(IP, Port, UUID) != 0)
+	// The function needs IP, Port and UUID as parameters
+    if (Kmbox.InitDevice("192.168.1.188", 16938, "264F1C27") != 0)
     {
         LOG_ERROR("Failed to initialize KMBOX");
 		return 1;
     }
     
     LOG("KMBOX initialized");
+
+	// That's it! You can now use the KMBOX API to control the device.
 
     system("pause");
     return 1;
