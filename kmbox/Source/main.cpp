99b7babe99e2983ccbfe97c18e0740c93a986cce
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-05-19 08:05:14 +0200

    Fix indentation and spacing in main.cpp

--- kmbox/Source/main.cpp
+++ kmbox/Source/main.cpp
@@ -5,16 +5,16 @@ int main()
 	SetConsoleTitleA("Debug Console");
 
     std::string IP = "192.168.1.188";
-	WORD Port = 0;
-	std::string UUID = "ABCDEFG";
+    WORD Port = 0;
+    std::string UUID = "ABCDEFG";
 
     if (Kmbox.InitDevice(IP, Port, UUID) != 0)
     {
         LOG_ERROR("Failed to initialize KMBOX");
 		return 1;
     }
-
-	LOG("KMBOX initialized");
+    
+    LOG("KMBOX initialized");
 
     system("pause");
 	return 1;
